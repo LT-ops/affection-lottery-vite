bcbe99150b3d759f467d96256b3b02c10ce2f723
LT-ops <157166833+LT-ops@users.noreply.github.com>
2025-04-12 14:12:21 -0400

    Initial commit with Vite setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MyDapp from './MyDapp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MyDapp />
+  </React.StrictMode>,
+) 
